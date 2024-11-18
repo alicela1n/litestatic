6ffc5fa47cdc320ac42e8f75e63f46c9f67b7a4b
Alice <4895631-alicela1n@users.noreply.gitlab.com>
2024-11-17 17:22:38 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Nanoweb
+Tiny lightweight static site generator written in Python that uses markdown and liquid to generate a website.
+
+# How it works
+The way it works is simple by design. The site is contained in the site directory, and the files in the site directory should be modifed for your website. The templates folder contains [liquid](https://shopify.github.io/liquid/) templates for generating the skeleton of the webpage. [Markdown](https://www.markdownguide.org/) is used for the webpage contents. The index.md is the markdown file that generates the front page of the website.
+
+You can put markdown files in the `site/` directory and have them generated as `site/(page name)`.
+You can also have blog-style posts by putting markdown files in the `site/posts/` directory.
+
+The name for the posts needs to be formatted as: `posts/YYYY-MM-DD-title.md`.
+
+The posts need to have a metadata header:
+```
+title: Post Title
+date: YYYY-MM-DD HH:mm
+```
+
+Posts will be indexed in `/posts` and you can link to the post index in your index.md.
+
+The `site/files/` directory is for any miscellaneous files that get copied to the output directory `out/`.
+
+The generated website is outputted to `out/` ready to be deployed to a web server or wherever.
---
+++ generate.py
@@ -0,0 +1,138 @@
+from marko import Markdown
+from pathlib import Path
+from liquid import Template
+from liquid import Environment
+from liquid import FileSystemLoader
+import os
+import shutil
+import frontmatter
+import datetime
+markdown = Markdown(extensions=['codehilite'])
+
+# Each post is a markdown file which gets parsed into an object which contains it's metadata, date,
+# and HTML content (converted from the Markdown).
+class Post:
+    def __init__(self, name, title, date, html):
+        self.name = name
+        self.title = title
+        self.date = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M')
+        self.html = html
+
+# generate_index_page(): Produce the index.html file which will be the front page of the website
+def generate_index_page(site_directory, templates_directory, output_directory):
+    env = Environment(loader=FileSystemLoader(f"{site_directory}/{templates_directory}/"))
+    template = env.get_template("index.html")
+
+    if not os.path.exists(f"{output_directory}"):
+        os.makedirs(f"{output_directory}")
+
+    with open(f"{site_directory}/index.md", "r") as index_file:
+        index_buffer = frontmatter.load(index_file)
+        index_body = markdown.convert(index_buffer.content)
+
+    with open(f"{output_directory}/index.html", "w") as output_file:
+        print(f"Generating {output_directory}/index.html")
+        output_file.write(template.render(index_body=index_body,))
+
+# generate_html_pages(): Produce the HTML pages out of markdown files that will make up the site
+def generate_html_pages(site_directory, templates_directory, output_directory):
+    env = Environment(loader=FileSystemLoader(f"{site_directory}/{templates_directory}/"))
+    template = env.get_template("page.html")
+
+    if not os.path.exists(f"{output_directory}"):
+        os.makedirs(f"{output_directory}")
+
+    page_files = Path(site_directory).glob("*.md")
+    for page in page_files:
+        with open(page, "r") as md_file:
+            page_buffer = frontmatter.load(md_file)
+            page_body = markdown.convert(page_buffer.content)
+            page_name = str(page.with_suffix(''))
+            page_name = page_name.partition('/')[2]
+
+        if not page_name == "index": # Exclude index
+            if not os.path.exists(f"{output_directory}/{page_name}"):
+                os.makedirs(f"{output_directory}/{page_name}")
+
+            with open(f"{output_directory}/{page_name}/index.html", "w") as output_file:
+                print(f"Generating {output_directory}/{page_name}/index.html")
+                output_file.write(template.render(page_body=page_body,))
+
+# generate_posts(): Produce an array of post objects
+def generate_posts(site_directory, posts_directory):
+    post_files = Path(f"{site_directory}/{posts_directory}").glob("*.md")
+    
+    posts = []
+    for post in post_files:
+        with open(post, "r") as md_file:
+            # Remove the extension from the post name
+            post_name = str(post.with_suffix(''))
+            # Remove the site_directory and post_directory from the post name
+            post_name = post_name.partition('/')[3]
+
+            post_buffer = frontmatter.load(md_file)
+            post_title = post_buffer['title']
+            post_date = post_buffer['date']
+            post_html = markdown.convert(post_buffer.content)
+
+            posts.append(Post(post_name, post_title, post_date, post_html))
+
+    return posts
+
+# generate_post_files(): Generate the html files for the posts
+def generate_post_files(posts, site_directory, templates_directory, posts_directory, output_directory):
+    env = Environment(loader=FileSystemLoader(f"{site_directory}/{templates_directory}/"))
+    template = env.get_template("post-template.html")
+
+    if not os.path.exists(f"{output_directory}/{posts_directory}"):
+        os.makedirs(f"{output_directory}/{posts_directory}")
+
+    for post in posts:
+        if not os.path.exists(f"{output_directory}/{posts_directory}/{post.name}"):
+            os.makedirs(f"{output_directory}/{posts_directory}/{post.name}")
+
+        with open(f"{output_directory}/{posts_directory}/{post.name}/index.html", "w") as output_file:
+            print(f"Generating {output_directory}/{posts_directory}/{post.name}")
+            output_file.write(template.render(post_title=post.title, post_date=post.date, post_html=post.html,))
+
+# generate_post_index(): Generate the post index
+def generate_post_index(posts, site_directory, templates_directory, posts_directory, output_directory):
+    posts_list = []
+
+    for post in posts:
+        posts_dict = {"title": post.title, "date": post.date, "path": f"{post.name}"} 
+        posts_list.append(posts_dict)
+    
+    posts_data = { "posts" : posts_list }
+
+    env = Environment(loader=FileSystemLoader(f"{site_directory}/{templates_directory}/"))
+    template = env.get_template("posts-index.html")
+
+    with open(f"{output_directory}/{posts_directory}/index.html", "w") as output_file:
+        print(f"Generating {output_directory}/{posts_directory}/index.html")
+        output_file.write(template.render(**posts_data))
+
+# copy_files_to_out(): Copy the files in the files directory to the output directory
+def copy_files_to_out(site_directory, files_directory, output_directory):
+    shutil.copytree(f"{site_directory}/{files_directory}", output_directory, symlinks=False, ignore=None, copy_function=shutil.copy2, ignore_dangling_symlinks=False, dirs_exist_ok=True)
+
+# main(): Main function
+def main():
+    output_directory = "out"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    site_directory = "site"
+    posts_directory = "posts"
+    files_directory = "files"
+    templates_directory = "templates"
+
+    generate_index_page(site_directory, templates_directory, output_directory)
+    generate_html_pages(site_directory, templates_directory, output_directory)
+    posts = generate_posts(site_directory, posts_directory)
+    generate_post_files(posts, site_directory, templates_directory, posts_directory, output_directory)
+    generate_post_index(posts, site_directory, templates_directory, posts_directory, output_directory)
+    copy_files_to_out(site_directory, files_directory, output_directory)
+
+if __name__ == '__main__':
+    main()
